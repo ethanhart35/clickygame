9265ea80fd8e9715800224b47be6dd3ddf6f27b8
Ethan Hart <ethanhart35@gmail.com>
2020-04-18 23:32:39 -0700

    built starting layout for the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Search from "./components/pages/Search";
+import Discover from "./components/pages/Discover";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={About} />
+        <Route path="/Discover" component={Discover} />
+        <Route exact path="/Search" component={Search} />
+      </div>
+    </Router>
   );
 }
 
