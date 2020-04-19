343ea28fbe54ad07d87f9ef80a5f3c7933b6790f
Ethan Hart <ethanhart35@gmail.com>
2020-04-18 23:43:02 -0700

    added component file

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+      A static welcome screen for visitors to the application.
+      </p>
+    </div>
+  );
+}
+
+export default About;
