ae20c2f7d76dedcdcda0523818a925e9fd0df8c4
Ethan Hart <ethanhart35@gmail.com>
2020-04-20 20:04:18 -0700

    added title

---
+++ build/precache-manifest.44b09b36b476e551e7e556f35cbf747b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9908116233e051dc93c4bd47aba1b5",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "dc30fc3ebf934dc17624",
+    "url": "/clickygame/static/js/2.6b33961e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clickygame/static/js/2.6b33961e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bc32e3a9c57d1aec5f",
+    "url": "/clickygame/static/js/main.98a1894a.chunk.js"
+  },
+  {
+    "revision": "c3472cfefc089e48f739",
+    "url": "/clickygame/static/js/runtime-main.5300a12d.js"
+  }
+]);
